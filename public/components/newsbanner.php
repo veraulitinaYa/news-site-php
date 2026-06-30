248fb2808827211e7aeef2b835ca840fc8ecef29
veraulitinaYa <veraulitina@yandex.ru>
2026-06-30 13:56:09 +0300

    feat: add data to banner

--- public/components/newsbanner.php
+++ public/components/newsbanner.php
@@ -1,13 +1,16 @@
 <div class="news-card news-card-banner-style">
       <img 
-    src="/images/0cdfb0183c985bea52e35b50e99f0909.jpg" 
+    src="/images/<?= htmlspecialchars($banner['image']) ?>"
     class="news-card-image news-card-banner-image-style" 
     alt="News image"
   />
     <div class="news-card-banner-text-style">
-        <h1 class="news-card-title news-card-banner-title-style"></h1>
+        <h1 class="news-card-title news-card-banner-title-style">
+          <?= htmlspecialchars($banner['title'] ?? '') ?>
+        </h1>
         <span
         class="news-card-announce news-card-banner-announce-style"
-        ></span>
+        >
+      <?= htmlspecialchars($banner['announce'] ?? '') ?></span>
     </div>
     </div>
